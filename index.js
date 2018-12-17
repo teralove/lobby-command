99066a1f267267908329041ea99976d3b197ed26
teralove <terascripts@gmail.com>
2018-12-16 20:04:52 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = function LobbyCommand(mod) {
 
-    command.add(['lobby', 'logout'], () => {
+    mod.command.add(['lobby', 'logout'], () => {
         mod.send('C_RETURN_TO_LOBBY', 1, {});
     });
 }
